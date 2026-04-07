38ea61f3e3133430b99c27be951402d296dc66a9
DenisDavidov-creator <denisdavidovforwork@gmail.com>
2026-04-07 13:26:50 +0700

    domain_recurrence

---
+++ internal/domain/task/recurrence.go
@@ -0,0 +1,53 @@
+package task
+
+import "time"
+
+type RecurrenceType string
+
+const (
+	TypeDaily         RecurrenceType = "daily"
+	TypeDayOfMonth    RecurrenceType = "day_of_month"
+	TypeEvenOdd       RecurrenceType = "even_odd"
+	TypeSpecificDates RecurrenceType = "specific_dates"
+)
+
+type EvenOddType string
+
+const (
+	TypeEven EvenOddType = "even"
+	TypeOdd  EvenOddType = "odd"
+)
+
+type Recurrence struct {
+	ID         int64          `json:"id"`
+	TaskID     int64          `json:"task_id"`
+	Type       RecurrenceType `json:"type"`
+	Interval   *int64         `json:"interval"`
+	DayOfMonth *int64         `json:"day_of_month"`
+	EvenOdd    *EvenOddType   `json:"even_odd"`
+	NextRunAt  *time.Time     `json:"next_run_at"`
+}
+
+type RecurrenceDates struct {
+	ID           int64     `json:"id"`
+	RecurrenceID int64     `json:"recurrence_id"`
+	Date         time.Time `json:"date"`
+}
+
+func (s RecurrenceType) Valid() bool {
+	switch s {
+	case TypeDaily, TypeDayOfMonth, TypeEvenOdd, TypeSpecificDates:
+		return true
+	default:
+		return false
+	}
+}
+
+func (s EvenOddType) Valid() bool {
+	switch s {
+	case TypeEven, TypeOdd:
+		return true
+	default:
+		return false
+	}
+}
